175446477ad650d24c9828a4c4429be3e8487d6a
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-02 14:29:46 -0500

    Done with All Bonuses

--- 1619370348__profile-page/profile-page/script.js
+++ 1619370348__profile-page/profile-page/script.js
@@ -4,6 +4,34 @@ var pn = document.querySelector("#profileName")
 function changeUserName(){
     // alert("HI THERE")
 
-    pn.innerHTML = "any other name"
+    pn.innerHTML = "Aeon Flux"
 }
 
+var numConnection = 418
+var numReq = 2;
+function removeCardY(ele){
+    // alert("Card")
+    var e = document.querySelector(".card-list")
+    e.removeChild(ele.parentNode.parentNode)
+    
+    var b1 = document.querySelector(".badge1")
+    var b2 = document.querySelector(".badge2")
+    numConnection++;
+    numReq--;
+    b1.innerHTML = numReq
+    b2.innerHTML = numConnection
+
+}
+
+function removeCardX(ele){
+    // alert("Card")
+    var e = document.querySelector(".card-list")
+    e.removeChild(ele.parentNode.parentNode)
+    
+    var b1 = document.querySelector(".badge1")
+    var b2 = document.querySelector(".badge2")
+    numReq--;
+    b1.innerHTML = numReq
+    b2.innerHTML = numConnection
+
+}
